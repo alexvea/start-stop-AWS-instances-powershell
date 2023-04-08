f8b3c2b591ce06215c0e3a6be7fde9c21f9278ff
alexvea <35368807+alexvea@users.noreply.github.com>
2023-04-08 21:56:56 +0200

    Create config_file.ps1
    First commit

---
+++ config_file.ps1
@@ -0,0 +1,9 @@
+## Parameters to edit before executing the script
+$UserName = "xxx"
+$AwsProfile = "xxxx"
+
+
+## Others parameters
+$SleepTime = 2
+$FolderName = ".\instances"
+$IconLocation = "C:\Windows\System32\Shell32.dll"
