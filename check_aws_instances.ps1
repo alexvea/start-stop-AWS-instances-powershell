8a974e1f645f392737154db105a2c71c64e8f1d5
alexvea <35368807+alexvea@users.noreply.github.com>
2023-04-10 12:24:36 +0200

    Update check_aws_instances.ps1
    Added comments.
    Added case to filter Tag Schedule value stop-only (to exclude shared instance that are usually with others values).
    Added case to create a shortcut to stop all the running instances of the specific User

--- check_aws_instances.ps1
+++ check_aws_instances.ps1
@@ -1,6 +1,7 @@
 #Import (unversioned) config_file.ps1
 . ./config_file.ps1
 
+
 ## Function to check if folder where shortcuts will be created exist or not.
 ## It will delete 
 function CreateFolder {
@@ -42,6 +43,7 @@ Write-Host "Creating shortcut for instance $InstanceName."
             {
                "stopped" {
                     $Action="start"
+
                     # key icon
                     $IconArrayIndex = 44
                }
@@ -80,14 +82,38 @@ function GetInstancesList {
         [Parameter(Mandatory = $true)]
         [string]$AwsProfile = $env:AWSPROFILE
     )
-Write-Host "## Getting AWS Instances for $UserName user. ##"
-aws ec2 describe-instances --filters Name=tag:User,Values=$UserName --profile $AwsProfile --output text --query "Reservations[*].Instances[*].{Instance_id:InstanceId,Name:Tags[?Key=='Name'].Value | [0],Instance_id:InstanceId,State:State.Name}" | %{ Create-instance-shortcut -InstanceName $_.split("`t")[1] -InstanceID $_.split("`t")[0] -InstanceStatus $_.split("`t")[2] }
 
+$MultipleInstancesStop=""
+Write-Host "## Getting AWS Instances for $UserName user. ##"
+Write-Host "## With tag Schedule stop-only (to exclude shared instances). ##"
+## List all instances with tag User=@Username@ and tag Schedule=stop-only. 
+# Example of output 
+# i-0c30xxxxx   avea-xxxxxxx   stopped
+# i-0a7xxxxxx   avea-old-x     stopped
+# i-04fbxxxxx   avea-old-xxxx  stopped
+# i-02xxxxxxxx  avea-old-xxxxx stopped
+# i-018xxxxx    avea-cent      running
+aws ec2 describe-instances --filters Name=tag:User,Values=$UserName Name=tag:Schedule,Values=stop-only --profile $AwsProfile --output text --query "Reservations[*].Instances[*].{Instance_id:InstanceId,Name:Tags[?Key=='Name'].Value | [0],Instance_id:InstanceId,State:State.Name}" | %{ 
+    Create-instance-shortcut -InstanceName $_.split("`t")[1] -InstanceID $_.split("`t")[0] -InstanceStatus $_.split("`t")[2]
+
+    #Case for multi-instances stop (from instance status) in the loop for each.
+    If($_.split("`t")[2] -eq "running") 
+        { 
+            $MultipleInstancesStop=$MultipleInstancesStop+" "+$_.split("`t")[0]
+        }   
+    # End of for each
+    }
+#Case for multi-instances stop (from instance status) outside the loop for each, to create the shortcut.
+if($MultipleInstancesStop -ne "")
+    {
+        Create-instance-shortcut -InstanceName all-running-instances-of-$UserName-stop-only -InstanceID $MultipleInstancesStop -InstanceStatus running
+    }
 }
+
 CreateFolder
 
 GetInstancesList -UserName $UserName -AwsProfile $AwsProfile
 
 Write-Host "## Closing in $SleepTime seconds...##"
 Start-Sleep $SleepTime
-##pause
+
